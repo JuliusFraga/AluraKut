2a58ebf7163810657e7491bcf3270596282105de
Júlio Fraga <jfragati@gmail.com>
2021-07-15 21:59:47 -0300

    Checkin inicial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
-import styled from 'styled-components'
+import MainGrid from './src/componentes/MainGrid'
+import Box from './src/componentes/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from './src/lib/AlurakutCommons'
+import {ProfileRelationsBoxWrapper} from './src/componentes/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(propriedades){
+  return (
+    <Box>
+      <img src={`http://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px'}}></img>      
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'JuliusFraga';
+  const pessoasFavoritas = [
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'juunegreiros',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+    <AlurakutMenu/>
+    <MainGrid>
+      <div className="profileArea" style={{gridArea: 'profileArea'}}>
+        <ProfileSideBar githubUser={githubUser}></ProfileSideBar>
+      </div>
+      <div className="welcomeArea" style={{gridarea: 'welcomeArea'}}>
+        <Box style={{ gridArea: 'welcomeArea' }}>
+          <h1 className="title">Bem Vindo(a)</h1>
+          <OrkutNostalgicIconSet></OrkutNostalgicIconSet>
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+          <h3 className="smalltitle">
+            Pessoas da Comunidade ({pessoasFavoritas.length})
+          </h3>
+          <ul>
+            {pessoasFavoritas.map((itemAtual) => {
+                  return(
+                    <li>
+                        <a href={`/user/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`}/>
+                        <span>{itemAtual}</span>
+                    </a>
+                    </li>
+                  )
+            })}
+            </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>
+    </MainGrid>
+    </>
+  )
 }
